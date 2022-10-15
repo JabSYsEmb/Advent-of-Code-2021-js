0bbe72efd9cf266ea6eabb30495583bc03488282
JabSysEmb <abduljabbar.sha97@gmail.com>
2022-10-15 13:01:05 +0300

    day3 part1

--- 3_day/index.js
+++ 3_day/index.js
@@ -1,6 +1,6 @@
 const data = require("./input.json");
 
-const _testcase = 
+const test = 
 	[
 		"00100",
 		"11110",
@@ -16,35 +16,22 @@ const _testcase =
 		"01010"
 	]
 
-const getGammaAndEpsilonRate = (arr) => {
-	let [gamma,epsilon] = ['0b','0b'];
+const part_1 = (input) => {
+	const counters = Array(input[0].length).fill(0)
 
-	const sum = Array(arr[0].length).fill(0);
-
-	arr.map(item => {
-		[...item].map((item,idx)=> {
-			if(item === '1'){
-				sum[idx] += 1;
-			}
-		})
+	input.map(item => {
+		[...item].map((item,idx) => {
+			item === '1' && counters[idx]++;
+		});
 	})
 
-	sum.map(item => {
-		if(item > arr.length/2) {
-			gamma += '1';
-			epsilon += '0';
-			return;
-		}
-		epsilon += '1';
-		gamma += '0';
-	})
+	const result = counters.reduce((result,item) => {
+		return item > input.length/2 ? [result[0].concat('1'),result[1].concat('0')]: [result[0].concat('0'),result[1].concat('1')];
+	},Array(2).fill('0b'))
 
-	return [gamma,epsilon]
+	const solution = result.map(item => Number(item)).reduce((multiply,item) => multiply*item ,1);
+
+	console.log(`soluton of part 1 is '${solution}'`);
 }
 
-const convertStringArrayIntoNumberArray = (str_arr) => str_arr.map(str => Number(str));
-
-const multiplyTwoNumber = ([num_1,num_2]) => num_1 * num_2;
-
-console.log(multiplyTwoNumber(convertStringArrayIntoNumberArray(getGammaAndEpsilonRate(data))));
-
+part_1(data)
