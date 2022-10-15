7a56c9828d27dab383f4cbddd89b24e9478fbb6f
JabSysEmb <abduljabbar.sha97@gmail.com>
2022-10-15 18:38:50 +0300

    part2 of day 3

--- 3_day/index.js
+++ 3_day/index.js
@@ -1,4 +1,4 @@
-const data = require("./input.json");
+const data = require('./input.json');
 
 const test = 
 	[
@@ -16,6 +16,15 @@ const test =
 		"01010"
 	]
 
+
+const dominated_bit_by_count = (count,length,result) => {
+	if (count > length/2) {
+		return [result[0].concat('1'), result[1].concat('0')];
+	}
+
+	return [result[0].concat('0'), result[1].concat('1')];
+}
+
 const part_1 = (input) => {
 	const counters = Array(input[0].length).fill(0)
 
@@ -25,13 +34,46 @@ const part_1 = (input) => {
 		});
 	})
 
-	const result = counters.reduce((result,item) => {
-		return item > input.length/2 ? [result[0].concat('1'),result[1].concat('0')]: [result[0].concat('0'),result[1].concat('1')];
-	},Array(2).fill('0b'))
+	const result = counters.reduce((result,count) => {
+		return dominated_bit_by_count(count,input.length,result);
+	},Array(2).fill('0b')) 
 
-	const solution = result.map(item => Number(item)).reduce((multiply,item) => multiply*item ,1);
+	const [gammaRate, epsilonRate] = result.map(item => Number(item));
 
-	console.log(`soluton of part 1 is '${solution}'`);
+	console.log(`soluton of part 1 is '${gammaRate * epsilonRate}'`);
 }
 
-part_1(data)
+const part_2 = (input) => {
+	[oxygen_generator_rates, co2_scrubber_rates] = input.reduce((arr,item) => { 
+		item[0] === '1' ? arr[0].push(item) : arr[1].push(item); 
+		return arr; 
+	}, [[],[]]);
+
+	if(co2_scrubber_rates.length > oxygen_generator_rates.length) {
+		[oxygen_generator_rates, co2_scrubber_rates] = [co2_scrubber_rates, oxygen_generator_rates];
+	}
+
+
+	for (let idx = 1; oxygen_generator_rates.length !== 1; ++idx){
+		let temp = oxygen_generator_rates.reduce((arr,item) => {
+			item[idx] === '1' ? arr[0].push(item):arr[1].push(item);
+			return arr;
+		}, [[],[]]);
+
+		oxygen_generator_rates = temp[0].length >= temp[1].length ? temp[0]:temp[1];
+	}
+
+	for (let idx = 1; co2_scrubber_rates.length !== 1; ++idx){
+		let temp = co2_scrubber_rates.reduce((arr,item) => {
+			item[idx] === '0' ? arr[0].push(item):arr[1].push(item);
+			return arr;
+		}, [[],[]]);
+
+		co2_scrubber_rates = temp[0].length <= temp[1].length ? temp[0]:temp[1];
+	}
+
+	console.log(`solution of part 2 is '${Number('0b' + oxygen_generator_rates[0]) * Number('0b' + co2_scrubber_rates[0])}'`)
+}
+
+part_1(data);
+part_2(data);
